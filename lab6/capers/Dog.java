adea014db929d0e3de58d3804789ac397e0a643b
ASmellyCat <joy.j.xu@outlook.com>
2022-10-28 00:13:11 +0800

    finished lab6

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -5,9 +5,9 @@ import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author ASmellyCat
 */
-public class Dog implements Serializable{ // TODO
+public class Dog implements Serializable{
 
     /** Folder that dogs live in. */
     static final File DOG_FOLDER = join(CapersRepository.CAPERS_FOLDER, "dogs"); // TODO (hint: look at the `join`
@@ -41,7 +41,6 @@ public class Dog implements Serializable{ // TODO
     public static Dog fromFile(String name) {
         File dogFile = join(DOG_FOLDER, name);
         return readObject(dogFile, Dog.class);
-        // TODO (hint: look at the Utils file)
     }
 
     /**
